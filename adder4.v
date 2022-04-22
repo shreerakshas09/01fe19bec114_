a331472f94deb41af52e8a3505d4fcef2daaff8d
shreerakshas09 <59998403+shreerakshas09@users.noreply.github.com>
2022-04-22 18:57:16 +0530

    Add files via upload

---
+++ adder4.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:15:56 04/10/2022 
+// Design Name: 
+// Module Name:    adder4 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module adder4(a,b,sum);
+
+input [3:0] a,b;
+output [3:0] sum;
+wire [3:0] sum;
+
+assign sum = a + b;
+
+endmodule
